03078b4d6c503e3c55bca9f926bc96addb61cf46
Vivek Tiwari <102945885+Vivek-Tiwari20@users.noreply.github.com>
2023-04-28 16:50:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS code here.
+	function checkSlide(e) {
+	const images = document.querySelectorAll(".slide-in");
+	
+	images.forEach((image) => {
+	const slideInAt =
+	window.scrollY + window.innerHeight - image.height / 2;
+	const imageBottom = image.offsetTop + image.height;
+	const isHalfShown = slideInAt > image.offsetTop;
+	const isNotScrolledPast = window.scrollY < imageBottom;
+	if (isHalfShown && isNotScrolledPast) {
+	image.classList.add("active");
+	} else {
+	image.classList.remove("active");
+	}
+	});
+	}
+	
